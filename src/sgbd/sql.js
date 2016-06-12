98a0140ad31b41cbba6d71beaea29ee72b947787
Ricardo Brambila <ricardobrambila.nave@gmail.com>
2016-06-12 11:58:32 -0300

    Added the from clause with aliases

--- src/sgbd/sql.js
+++ src/sgbd/sql.js
@@ -1,9 +1,9 @@
 var SQL = (function() {
     function Commands(sql) {
-        this.sql = sql || ""
+        this.sql = sql || "";
     };
 
-    Commands.prototype.select = function(columns) {
+    Commands.prototype.select = function(columns, alias) {
         columns = (typeof columns === "undefined") ? ['*'] : columns;
 
         this.sql += "SELECT " + columns.join(", ");
@@ -11,15 +11,28 @@ var SQL = (function() {
         return this;
     };
 
-    Commands.prototype.from = function(tablename) {
-        this.sql += " from " + tablename;
+    Commands.prototype.from = function(tablenames, alias) {
+        this.sql += " FROM ";
+        var aliasPart = undefined;
+
+        for (var i = 0; i < tablenames.length; i++) {
+            // If theres an alias, append it to the end of the string
+            aliasPart = (alias && alias[i] ? (" " + alias[i] + ", ") : ", ");
+            this.sql += tablenames[i] + "" + aliasPart;
+        }
+
+        // Remove last comma
+        this.sql = this.sql.replace(/, (?=[^,]*$)/, '');
+        return this;
+    };
+
+    Commands.prototype.where = function() {
+
     };
 
     Commands.prototype.build = function() {
         return this.sql;
     };
-
-    var commands = new Commands();
 
     return Commands;
 })();
